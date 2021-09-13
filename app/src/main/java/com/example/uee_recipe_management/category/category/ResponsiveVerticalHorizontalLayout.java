29185fc871bf64230f9136ed54f878efeb6d367c
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-13 10:46:36 +0530

    View Header Created, Added to Main view.

--- app/src/main/java/com/example/uee_recipe_management/category/category/ResponsiveVerticalHorizontalLayout.java
+++ app/src/main/java/com/example/uee_recipe_management/category/category/ResponsiveVerticalHorizontalLayout.java
@@ -3,6 +3,7 @@ package com.example.uee_recipe_management.category.category;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.uee_recipe_management.category.R;
 
@@ -11,6 +12,7 @@ public class ResponsiveVerticalHorizontalLayout extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_responsive_vertical_horizontal_layout);
     }
 }
