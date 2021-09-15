55d844531d0a09f22d66432b8e764d94d210b55b
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-16 02:43:53 +0530

    Search Layout Search Function implemented. With EditText.
    Signed-off-by: AnawaratneMANA <it19086408@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/category/adapter/CategoryItemSearchAdapter.java
+++ app/src/main/java/com/example/uee_recipe_management/application/category/adapter/CategoryItemSearchAdapter.java
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.uee_recipe_management.application.R;
 import com.example.uee_recipe_management.application.category.model.CategoryItem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryItemSearchAdapter extends RecyclerView.Adapter<CategoryItemSearchAdapter.SearchViewHolder> {
@@ -62,6 +63,11 @@ public class CategoryItemSearchAdapter extends RecyclerView.Adapter<CategoryItem
     @Override
     public int getItemCount() {
         return categorySearchItemList.size();
+    }
+
+    public void filterList(ArrayList<CategoryItem> filteredList){
+        categorySearchItemList = filteredList;
+        notifyDataSetChanged();
     }
 
     public static final class SearchViewHolder extends RecyclerView.ViewHolder {
