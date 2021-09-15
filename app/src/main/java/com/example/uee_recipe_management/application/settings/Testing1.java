2900f84cbb5d004322682de1cf011a821ea237d5
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-16 01:58:33 +0530

    testing list view
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

---
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/Testing1.java
@@ -0,0 +1,16 @@
+package com.example.uee_recipe_management.application.settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uee_recipe_management.application.R;
+
+public class Testing1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_testing2);
+    }
+}
