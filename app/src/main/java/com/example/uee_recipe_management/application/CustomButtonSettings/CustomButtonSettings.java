7e82ce7029efc8b829d50d9a77f59ce28abf55db
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-16 01:21:12 +0530

    initial codes for CustomButtonSettings
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/CustomButtonSettings/CustomButtonSettings.java
+++ app/src/main/java/com/example/uee_recipe_management/application/CustomButtonSettings/CustomButtonSettings.java
@@ -1,15 +1,34 @@
 package com.example.uee_recipe_management.application.CustomButtonSettings;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
+
 import com.example.uee_recipe_management.application.R;
 
-public class CustomButtonSettings extends AppCompatActivity {
+public class CustomButtonSettings extends BaseAdapter {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_custom_button_settings);
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
     }
 }
