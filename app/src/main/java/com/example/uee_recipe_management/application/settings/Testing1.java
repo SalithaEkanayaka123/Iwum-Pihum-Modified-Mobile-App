4cff87a19804dd4d0ab8a799b5916dcc42d7e12b
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-16 20:17:48 +0530

    Issue in listview
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/Testing1.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/Testing1.java
@@ -2,7 +2,10 @@ package com.example.uee_recipe_management.application.settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.uee_recipe_management.application.CustomButtonSettings.CustomButtonSettings;
@@ -35,5 +38,16 @@ public class Testing1 extends AppCompatActivity {
         listView1.setAdapter(adapter);
         //mentioning to update the rows
         adapter.notifyDataSetChanged();
+
+        listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println(i);
+                System.out.println(l);
+                Intent intent = new Intent(Testing1.this , SettingPage.class);
+                //navigating to the relevent activity
+                startActivity(intent);
+            }
+        });
     }
 }
