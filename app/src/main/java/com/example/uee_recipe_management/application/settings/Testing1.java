c15ae2c54fc9053d403a71bff7f3fa9a9ce0cd14
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-16 10:07:19 +0530

    Checking include tag
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/Testing1.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/Testing1.java
@@ -3,11 +3,18 @@ package com.example.uee_recipe_management.application.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.uee_recipe_management.application.CustomButtonSettings.CustomButtonSettings;
 import com.example.uee_recipe_management.application.R;
+
+import java.util.ArrayList;
 
 public class Testing1 extends AppCompatActivity {
 
+    ListView listView1, listView2;
+    CustomButtonSettings adapter;//declaring the Custom adapter
+    //ArrayList<SongList> array;//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
