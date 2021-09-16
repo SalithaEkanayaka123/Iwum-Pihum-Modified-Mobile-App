8c728c5bd9858b4c0d89a4967ee7d4dbdf540ed7
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-16 12:07:26 +0530

    Created the model class for the notifications
    Signed-off-by: AnawaratneMANA <it19086408@my.sliit.lk>

---
+++ app/src/main/java/com/example/uee_recipe_management/application/category/adapter/NotificationRecyclerAdapter.java
@@ -0,0 +1,11 @@
+package com.example.uee_recipe_management.application.category.adapter;
+
+public class NotificationRecyclerAdapter {
+    /**
+     * This is a multitype adapter class for displaying notifications.
+     * */
+
+
+
+
+}
