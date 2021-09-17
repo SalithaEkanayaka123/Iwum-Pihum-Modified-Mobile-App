8ffa102ca76e7d9de84b0e05e4680a5217585461
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-17 15:59:27 +0530

    Background interface added
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

---
+++ app/src/main/java/com/example/uee_recipe_management/application/Background_Settings.java
@@ -0,0 +1,14 @@
+package com.example.uee_recipe_management.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Background_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_background__settings);
+    }
+}
---
+++ app/src/main/java/com/example/uee_recipe_management/application/PrivacyAndPolicy_Settings.java
@@ -0,0 +1,14 @@
+package com.example.uee_recipe_management.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrivacyAndPolicy_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_and_policy__settings);
+    }
+}
