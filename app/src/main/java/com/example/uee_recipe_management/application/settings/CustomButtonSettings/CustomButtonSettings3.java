50630061c80070b646cc621eae8b5df0c0035ed3
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-17 18:16:20 +0530

    New card view updated
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/CustomButtonSettings/CustomButtonSettings3.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/CustomButtonSettings/CustomButtonSettings3.java
@@ -2,11 +2,19 @@ package com.example.uee_recipe_management.application.settings.CustomButtonSetti
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.uee_recipe_management.application.R;
+import com.example.uee_recipe_management.application.settings.model.customButton1;
+
+import java.util.ArrayList;
 
 public class CustomButtonSettings3 extends AppCompatActivity {
+    private Context context;
+    ArrayList<customButton1> list;
+    TextView title1, title2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
