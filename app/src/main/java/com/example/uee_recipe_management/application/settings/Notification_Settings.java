b33647d170a9594fdcc05b07f03c1483e6a19226
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-17 17:39:12 +0530

    Notification java class updated
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/Notification_Settings.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/Notification_Settings.java
@@ -3,14 +3,46 @@ package com.example.uee_recipe_management.application.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.uee_recipe_management.application.R;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings2;
+import com.example.uee_recipe_management.application.settings.model.customButton1;
+
+import java.util.ArrayList;
 
 public class Notification_Settings extends AppCompatActivity {
+
+    ListView listView1, listView2;
+    CustomButtonSettings adapter;
+    CustomButtonSettings2 adapter2;//declaring the Custom adapter
+    ArrayList<customButton1> array, array2, array3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification__settings);
+
+        listView1 = (ListView) findViewById(R.id.listSettings1);
+        listView2 = (ListView) findViewById(R.id.listSettings2);
+
+        array = new ArrayList<>();
+        array.add(new customButton1("background1", "background2", true));
+        array.add(new customButton1("background1", "background2", true));
+        array.add(new customButton1("background1", "background2", true));
+
+        array2 = new ArrayList<>();
+        array2.add(new customButton1("background11", "background22", true));
+        array2.add(new customButton1("background11", "background22", true));
+        array2.add(new customButton1("background11", "background22", true));
+
+
+
+        adapter = new CustomButtonSettings(this , array);//this is called in customMusicAdapter
+        adapter2 = new CustomButtonSettings2(this , array2);//
+        //then set that adapter to the listView
+        listView1.setAdapter(adapter);
+        listView2.setAdapter(adapter2);
     }
 }
