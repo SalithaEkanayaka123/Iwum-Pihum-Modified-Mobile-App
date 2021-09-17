ff5ec85577a70af43b7cd23ab214fb725eaeb01c
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-17 18:47:00 +0530

    java class updated
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/PrivacyAndPolicy_Settings.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/PrivacyAndPolicy_Settings.java
@@ -3,14 +3,37 @@ package com.example.uee_recipe_management.application.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.uee_recipe_management.application.R;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings2;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings3;
+import com.example.uee_recipe_management.application.settings.model.customButton1;
+
+import java.util.ArrayList;
 
 public class PrivacyAndPolicy_Settings extends AppCompatActivity {
+    ListView listView1;
+    CustomButtonSettings3 adapter2;//declaring the Custom adapter
+    ArrayList<customButton1> array;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_and_policy__settings);
+
+        listView1 = (ListView) findViewById(R.id.listSettings9);
+
+        array = new ArrayList<>();
+        array.add(new customButton1("Information Collection and Use", "Handles Background Settings", true));
+        array.add(new customButton1("Log Data", "Handles Background Settings", true));
+        array.add(new customButton1("Cookies", "Handles Background Settings", true));
+        array.add(new customButton1("Service Providers", "Handles Background Settings", true));
+        array.add(new customButton1("Security", "Handles Background Settings", true));
+
+        adapter2 = new CustomButtonSettings3(this , array);//
+        //then set that adapter to the list
+        listView1.setAdapter(adapter2);
     }
 }
