448bd17c8cae7a0fcc81dede738a4dbd2e56ab6b
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-18 05:45:00 +0530

    Defined and Registered the UI element in the Splash Screen.
    Signed-off-by: AnawaratneMANA <it19086408@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/splash/OpeningSplashScreen.java
+++ app/src/main/java/com/example/uee_recipe_management/application/splash/OpeningSplashScreen.java
@@ -3,14 +3,35 @@ package com.example.uee_recipe_management.application.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.uee_recipe_management.application.R;
 
 public class OpeningSplashScreen extends AppCompatActivity {
 
+    //Variables
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView heading, subheading;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_opening_splash_screen);
+
+        //Registering the Anmiations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.logo);
+        heading = findViewById(R.id.title_header);
+        subheading = findViewById(R.id.title_subheader);
     }
 }
