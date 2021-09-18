1209cd751d1ec29cee2ba2f763a30af5860668e6
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-18 15:06:11 +0530

    add header

---
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/ItemTesting.java
@@ -0,0 +1,17 @@
+package com.example.uee_recipe_management.application.bookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uee_recipe_management.application.R;
+
+public class ItemTesting extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_testing);
+    }
+}
