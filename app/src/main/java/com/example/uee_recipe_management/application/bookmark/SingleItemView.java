168fb0a18bcd5b43b4e59747eff06763f77d3846
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-24 11:15:17 +0530

    add single item interface

---
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
@@ -0,0 +1,18 @@
+package com.example.uee_recipe_management.application.bookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uee_recipe_management.application.R;
+
+public class SingleItemView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_item_view);
+
+
+    }
+}
