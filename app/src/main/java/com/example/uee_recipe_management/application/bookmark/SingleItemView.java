13118ce4b3d5bb78b2fbcd7f4fa2205f68b22304
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-24 22:24:11 +0530

    added slider adapter

--- app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
@@ -1,32 +1,20 @@
 package com.example.uee_recipe_management.application.bookmark;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
-//import com.denzcoskun.imageslider.ImageSlider;
-//import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.uee_recipe_management.application.R;
+import com.smarteist.autoimageslider.SliderView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class SingleItemView extends AppCompatActivity {
 
-    //ImageSlider imageSlider;
+    SliderView sliderView;
+    int[] images = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_item_view);
 
-//        imageSlider = findViewById(R.id.slider);
 
-       // List<SlideModel> slideModels = new ArrayList<>();
-//        slideModels.add(new SlideModel("","1 Image"));
-//        slideModels.add(new SlideModel("","2 Image"));
-//        slideModels.add(new SlideModel("","3 Image"));
-//        slideModels.add(new SlideModel("","4 Image"));
-//        imageSlider.setImageList(slideModels,true);
     }
 }
