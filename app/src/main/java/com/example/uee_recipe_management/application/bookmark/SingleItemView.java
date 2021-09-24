428362166b7ac3e1505bb87a24997e9e8e109ddf
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-25 01:37:17 +0530

    single item view image slider added

--- app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
@@ -2,6 +2,8 @@ package com.example.uee_recipe_management.application.bookmark;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.uee_recipe_management.application.R;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
 
@@ -14,7 +16,14 @@ public class SingleItemView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_item_view);
+        sliderView = findViewById(R.id.image_slider);
 
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
 
     }
 }
