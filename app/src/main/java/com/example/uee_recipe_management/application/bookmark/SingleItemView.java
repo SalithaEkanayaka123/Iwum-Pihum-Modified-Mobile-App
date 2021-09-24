69dd0707b85afd64dcb9d4e936d046fe5b11f9d3
Taneesha <taneesha.chandrasena@acentura.com>
2021-09-24 21:34:33 +0530

    add image slider view

--- app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/SingleItemView.java
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//import com.denzcoskun.imageslider.ImageSlider;
+//import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.uee_recipe_management.application.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SingleItemView extends AppCompatActivity {
+
+    //ImageSlider imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_item_view);
 
+//        imageSlider = findViewById(R.id.slider);
 
+       // List<SlideModel> slideModels = new ArrayList<>();
+//        slideModels.add(new SlideModel("","1 Image"));
+//        slideModels.add(new SlideModel("","2 Image"));
+//        slideModels.add(new SlideModel("","3 Image"));
+//        slideModels.add(new SlideModel("","4 Image"));
+//        imageSlider.setImageList(slideModels,true);
     }
 }
