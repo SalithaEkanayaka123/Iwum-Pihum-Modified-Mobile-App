9c0686dd8fb445468276f2efbc2d684d1a458dd3
SalithaEkanaya <it19121048@my.sliit.lk>
2021-09-24 11:27:54 +0530

    display language java class updated
    Signed-off-by: SalithaEkanaya <it19121048@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/settings/DisplayLanguage.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/DisplayLanguage.java
@@ -8,15 +8,38 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.uee_recipe_management.application.R;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings;
+import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings4;
+import com.example.uee_recipe_management.application.settings.model.customButton1;
+
+import java.util.ArrayList;
 
 public class DisplayLanguage extends DialogFragment {
+
+    ListView listView1;
+    CustomButtonSettings4 adapter2;//declaring the Custom adapter
+    ArrayList<customButton1> array;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_language, container, false);
+        View v1 = inflater.inflate(R.layout.fragment_display_language, container, false);
+        listView1 = (ListView) v1.findViewById(R.id.listSettings12);
+
+        array = new ArrayList<>();
+        array.add(new customButton1("English", "Automatic background sync", true));
+        array.add(new customButton1("Sinhala", "sync on startup", true));
+
+
+        adapter2 = new CustomButtonSettings4(getContext() , array);//
+        //then set that adapter to the list
+        listView1.setAdapter(adapter2);
+
+
+        return v1;
     }
 }
