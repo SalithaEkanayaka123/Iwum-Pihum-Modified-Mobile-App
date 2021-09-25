33995c821ebc085ef0a23f8428019cf6adbf1b1b
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 21:36:05 +0530

    UI Element defined XML file updated.
    Signed-off-by: AnawaratneMANA <it19086408@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/item/Item.java
+++ app/src/main/java/com/example/uee_recipe_management/application/item/Item.java
@@ -6,10 +6,14 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 import com.example.uee_recipe_management.application.R;
+import com.smarteist.autoimageslider.SliderView;
 
 import java.util.Objects;
 
 public class Item extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3,};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
