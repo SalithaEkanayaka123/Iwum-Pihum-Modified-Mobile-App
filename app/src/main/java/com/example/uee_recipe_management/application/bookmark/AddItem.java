c337253bf035eb4eceead1118345378a5b2be8f5
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-25 21:08:56 +0530

    Tracking local changers IT19086408
    Signed-off-by: AnawaratneMANA <it19086408@my.sliit.lk>

--- app/src/main/java/com/example/uee_recipe_management/application/bookmark/AddItem.java
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/AddItem.java
@@ -24,7 +24,7 @@ public class AddItem extends AppCompatActivity {
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent  = new Intent(view.getContext(), ClipData.Item.class);
+                Intent intent  = new Intent(view.getContext(), SingleItemView.class);
                 view.getContext().startActivity(intent);
             }
         });
