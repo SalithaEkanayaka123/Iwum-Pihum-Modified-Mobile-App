ab46c3e8537500598fc88a6a44119f499d41e627
AnawaratneMANA <it19086408@my.sliit.lk>
2021-10-08 23:24:39 +0530

    Picasso Dependency added to the project.

--- app/src/main/java/com/example/uee_recipe_management/application/bookmark/AddItem.java
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/AddItem.java
@@ -1,21 +1,50 @@
 package com.example.uee_recipe_management.application.bookmark;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 import com.example.uee_recipe_management.application.R;
 
 public class AddItem extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private Uri imageUri;
+
     Button addItem;
+    Button attachImage;
+    ProgressBar progressBar;
+    EditText name;
+    EditText subName;
+    EditText description;
+    ImageView attachImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        addItem = findViewById(R.id.button_add);
+        addItem = findViewById(R.id.add_recipe_button);
+        attachImage = findViewById(R.id.attach_image_button);
+        attachImageView = findViewById(R.id.attach_image);
+        progressBar = findViewById(R.id.progressBar);
+        name = findViewById(R.id.recipe_name);
+        subName = findViewById(R.id.sub_name);
+        description = findViewById(R.id.description);
+
+        attachImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
 
         addItem.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +53,23 @@ public class AddItem extends AppCompatActivity {
                 view.getContext().startActivity(intent);
             }
         });
+    }
+
+    //Old Method.
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            Picasso.with(this).load(imageUri).into(attachImageView);
+        }
     }
 }
