cb9ddf99859d6f272ff10f849fda37132a36f5ad
SalithaEkanaya <it19121048@my.sliit.lk>
2021-10-09 19:26:38 +0530

    changes added

--- app/src/main/java/com/example/uee_recipe_management/application/settings/RingingTones.java
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/RingingTones.java
@@ -3,6 +3,8 @@ package com.example.uee_recipe_management.application.settings;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
@@ -10,13 +12,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.uee_recipe_management.application.R;
 import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings4;
 import com.example.uee_recipe_management.application.settings.CustomButtonSettings.CustomButtonSettings5;
+import com.example.uee_recipe_management.application.settings.model.Audio;
 import com.example.uee_recipe_management.application.settings.model.RingingTone;
 import com.example.uee_recipe_management.application.settings.model.customButton1;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,28 +36,56 @@ public class RingingTones extends DialogFragment {
 
     ListView listView1;
     CustomButtonSettings5 adapter2;//declaring the Custom adapter
-    ArrayList<RingingTone> array;
+    ArrayList<Audio> array;
     private MediaPlayer mediaPlayer;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    Audio audio;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v1 = inflater.inflate(R.layout.fragment_ringing_tones, container, false);
         listView1 = (ListView) v1.findViewById(R.id.listSettings13);
-        mediaPlayer = new MediaPlayer();
+        audio = new Audio();
+//        ArrayAdapter
+            array = new ArrayList<>();
+//        array.add(new RingingTone("RingingTone1", R.raw.music1));
+//        array.add(new RingingTone("RingingTone2", R.raw.music2));
+//        array.add(new RingingTone("RingingTone3", R.raw.music3));
+//        array.add(new RingingTone("RingingTone4", R.raw.music4));
+//        array.add(new RingingTone("RingingTone5", R.raw.music5));
+//        array.add(new RingingTone("Stop", R.raw.music5));
 
-        array = new ArrayList<>();
-        array.add(new RingingTone("RingingTone1", R.raw.music1));
-        array.add(new RingingTone("RingingTone2", R.raw.music2));
-        array.add(new RingingTone("RingingTone3", R.raw.music3));
-        array.add(new RingingTone("RingingTone4", R.raw.music4));
-        array.add(new RingingTone("RingingTone5", R.raw.music5));
-        array.add(new RingingTone("Stop", R.raw.music5));
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Audio");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds:snapshot.getChildren()) {
+                    System.out.println("fff " + ds);
+                    audio = ds.getValue(Audio.class);
+                    array.add(audio);
+                }
+                adapter2 = new CustomButtonSettings5(getContext() , array);
+                //then set that adapter to the list
+                listView1.setAdapter(adapter2);
+//                String value = snapshot.getValue(Audio.class).toString();
+//                //array.add(new Audio(value, value));
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
-        adapter2 = new CustomButtonSettings5(getContext() , array);
-        //then set that adapter to the list
-        listView1.setAdapter(adapter2);
+//        System.out.println("ssss " + databaseReference);
+
+
         listView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -87,4 +126,5 @@ public class RingingTones extends DialogFragment {
         });
         return v1;
     }
+
 }
---
+++ app/src/main/java/com/example/uee_recipe_management/application/settings/model/Audio.java
@@ -0,0 +1,22 @@
+package com.example.uee_recipe_management.application.settings.model;
+
+public class Audio {
+    String title, audio;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+}
