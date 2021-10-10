5cc88a187cc067206d2002dd019d29e1032e3dee
AnawaratneMANA <it19086408@my.sliit.lk>
2021-10-10 16:35:26 +0530

    Empty Fragment Added.

---
+++ app/src/main/java/com/example/uee_recipe_management/application/bookmark/ItemTestingFragment.java
@@ -0,0 +1,37 @@
+package com.example.uee_recipe_management.application.bookmark;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uee_recipe_management.application.R;
+
+
+public class ItemTestingFragment extends Fragment {
+
+    public static ItemTestingFragment newInstance(String param1, String param2) {
+        ItemTestingFragment fragment = new ItemTestingFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_testing, container, false);
+    }
+}
