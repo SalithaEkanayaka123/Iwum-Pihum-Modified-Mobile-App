5d2c02e2544a3783494a180d788363208d0a4ccf
MDilankaN <it19104690@my.sliit.lk>
2021-10-10 05:34:32 +0530

    home recycle view complete

--- app/src/main/java/com/example/uee_recipe_management/application/home/Home.java
+++ app/src/main/java/com/example/uee_recipe_management/application/home/Home.java
@@ -2,23 +2,32 @@ package com.example.uee_recipe_management.application.home;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.uee_recipe_management.application.NavgationController;
 import com.example.uee_recipe_management.application.R;
-import com.example.uee_recipe_management.application.category.CategorySearchLayout;
-import com.example.uee_recipe_management.application.notification.NotificationLayout;
-import com.example.uee_recipe_management.application.settings.SettingPage;
+import com.example.uee_recipe_management.application.category.adapter.CategoryItemSearchAdapter;
+import com.example.uee_recipe_management.application.category.model.CategoryItem;
+import com.example.uee_recipe_management.application.home.Adapter.HomeAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
+
+    RecyclerView longCardRecyclerView;
+    HomeAdapter homeAdapter;
+
 
     NavgationController Nav = new NavgationController();
     @Override
@@ -31,6 +40,18 @@ public class Home extends AppCompatActivity {
         Menu menu = bottomNavView.getMenu();
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
+
+
+        //Add items to the category item model class.
+        List<CategoryItem> categoryItems = new ArrayList<>();
+        categoryItems.add(new CategoryItem("Fried Rice", R.drawable.image_1, "This is sample description"));
+        categoryItems.add(new CategoryItem("Chicken Kottu", R.drawable.image_2, "This is sample description"));
+        categoryItems.add(new CategoryItem("Soup", R.drawable.image_3, "This is sample description"));
+        categoryItems.add(new CategoryItem("Coca Cola", R.drawable.image_4, "This is sample description"));
+        setRecyclerHome(categoryItems);
+
+
+
     }
 
     private  BottomNavigationView.OnNavigationItemSelectedListener navListener = new
@@ -42,4 +63,12 @@ public class Home extends AppCompatActivity {
                     return true;
                 }
     };
+
+    private void setRecyclerHome(List<CategoryItem> hItems){
+        longCardRecyclerView = findViewById(R.id.home_page_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        longCardRecyclerView.setLayoutManager(layoutManager);
+        homeAdapter = new HomeAdapter(this, hItems);
+        longCardRecyclerView.setAdapter(homeAdapter);
+    }
 }
